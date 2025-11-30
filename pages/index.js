4e3762201d1d4447d1e30e25f42e872206d396dd
Your Name <henriquefernandesleite@gmail.com>
2025-11-29 23:58:30 -0300

    alteração do meu website

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
-import { useState } from "react";
-
 function Home() {
 
   return (
     <>
-        <h1>Camilinha do meu coração, eu te amo de montão</h1>
-        <h2>Se você também me ama me ama, da um sorrizinho</h2>
-        <p>Esse "site" está visivel para o mundo inteiro ver, inclusive você</p>
+         <h1>Camilão, me da seu biscoitão</h1>
     </>
   );
 }
