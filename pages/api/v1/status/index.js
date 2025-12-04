b484e0401d554e474675f8a674fc0b6a140c0e7e
Your Name <henriquefernandesleite@gmail.com>
2025-12-03 22:56:38 -0300

    Configurando variáveis de ambiente e ajustando o compose.yml

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,11 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
   response.status(200).json({
     nome: "Henrique",
-    sobrenome: "Fernandes",
   });
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
 }
 
 export default status;
