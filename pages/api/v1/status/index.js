d64ce9e7dab37c47968d7d7e6810075e55172f22
Your Name <henriquefernandesleite@gmail.com>
2025-12-05 11:21:58 -0300

    fix: protected sql injection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,31 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  response.status(200).json({
-    nome: "Henrique",
+  const updateAt = new Date().toISOString();
+  const dbVersion = await database.query("SELECT version();");
+  const maxConns = await database.query(
+    "SELECT current_setting('max_connections')::int AS max_connections;",
+  );
+  const databaseName = process.env.POSTGRES_DB;
+  const activeConns = await database.query({
+    text: "SELECT count(*)::int AS conexoes_ativas FROM pg_stat_activity WHERE datname=$1",
+    values: [databaseName],
   });
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+
+  const resultDbVersion = dbVersion.rows[0].version.match(/\d+\.\d+/)[0];
+  const resultMaxConns = maxConns.rows[0].max_connections;
+  const resultActiveConns = activeConns.rows.length;
+
+  response.status(200).json({
+    update_At: updateAt,
+    dependecies: {
+      database: {
+        version: resultDbVersion,
+        max_connections: parseInt(resultMaxConns),
+        resultActiveConns: resultActiveConns,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,10 @@
 test("GET to /api/v1/status shold return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  const parseUpdatedAt = new Date(responseBody.update_At).toISOString();
+  expect(responseBody.update_At).toEqual(parseUpdatedAt);
+  expect(responseBody.dependecies.database.version).toEqual("16.0");
+  expect(responseBody.dependecies.database.max_connections).toEqual(100);
+  expect(responseBody.dependecies.database.resultActiveConns).toBe(1);
 });
