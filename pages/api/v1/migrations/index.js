d1a17b02664df717c613be7ac422e9911c198a7f
Your Name <henriquefernandesleite@gmail.com>
2025-12-11 14:07:22 -0300

    Refatorando e reaproveitando code base

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,29 +2,25 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 async function status(request, response) {
+  const defaultMigrationsOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
   if (request.method === "GET") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
-
-    console.log("entrou no get");
-
+    const migrations = await migrationRunner(defaultMigrationsOptions);
     return response.status(200).json(migrations);
   }
+
   if (request.method === "POST") {
     const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+      ...defaultMigrationsOptions,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     });
+
     console.log("entrou no post");
     return response.status(200).json(migrations);
   }
