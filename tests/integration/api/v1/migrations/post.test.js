5118035f36ed4f5710aa598f3dad5482cb51de4a
riquefernandes <henriquefernandesleite@gmail.com>
2025-12-16 13:42:50 -0300

    create: orchestrator for all services test

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,10 +1,10 @@
 import database from "infra/database";
+import orquestrator from "tests/orquestrator.js";
 
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orquestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public");
-}
-
-beforeAll(cleanDatabase);
+});
 
 test("Post migrations to /api/v1/status shold return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
@@ -14,5 +14,3 @@ test("Post migrations to /api/v1/status shold return 200", async () => {
   const response1Body = await response1.json();
   expect(Array.isArray(response1Body)).toBe(true);
 });
-
-// teste
